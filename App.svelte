05d5e97428ae7f2609e7a6130b378b6fc6ab05fd
Casey <caseyros@gmail.com>
2020-03-11 10:14:31 +0000

    prefetch comments; preprocess replies datastructures

--- App.svelte
+++ App.svelte
@@ -2,7 +2,7 @@
     section(class='popular-stories-list')
         nav
             +each('popular_stories as story')
-                article(on:click!='{() => {fetch_base_comments_for_story(story.id); selected_story = story; read_stories.add(story.id); read_stories = read_stories}}' class:read-story-brochure='{read_stories.has(story.id)}' class:selected-story-brochure='{selected_story.id === story.id}' class='story-brochure')
+                article(on:click!='{() => {selected_story = story; read_stories.add(story.id); read_stories = read_stories}}' class:read-story-brochure='{read_stories.has(story.id)}' class:selected-story-brochure='{selected_story.id === story.id}' class='story-brochure')
                     section
                         section(class='story-headline') {story.title}
                         section(class='subreddit-label') {story.subreddit}
@@ -10,12 +10,11 @@
                         +if('story.thumbnail !== "default" && story.thumbnail !== "self"')
                             img(class='story-thumbnail' src='{story.thumbnail}')
         article(class='popular-story-reader')
-            +each('selected_story_comments as comment')
+            +each('selected_story.replies as comment')
                 p(class='comment') {comment.body}
 </template>
 
 <style type="text/stylus">
-
     .popular-stories-list
         display: flex
         height: 100%
@@ -26,6 +25,7 @@
         overflow: auto
     .comment
         font-size: 12px
+        line-height: 1.2
         padding: 6px
         word-break: break-word
     nav
@@ -41,6 +41,7 @@
         background: lightgray
     .selected-story-brochure
         background: wheat
+        border: 1px solid black
     .story-headline
         max-height: 56px
         overflow: hidden
@@ -61,8 +62,8 @@
 <script type="text/coffeescript">
     export popular_stories = []
     export niche_stories = []
-    export selected_story = {}
-    export selected_story_comments = []
+    export selected_story =
+        replies: []
     export read_stories = new Set()
     # docs: https://github.com/reddit-archive/reddit/wiki/OAuth2#application-only-oauth
     # docs: https://www.reddit.com/dev/api
@@ -79,20 +80,34 @@
             body
         })
         { token_type, access_token } = await response.json()
-        response = await fetch('https://oauth.reddit.com/best', {
+        response = await fetch('https://oauth.reddit.com/best?limit=10', {
             method: 'GET'
             headers:
                 'Authorization': token_type + ' ' + access_token
         })
         { data } = await response.json()
-        popular_stories = data.children.map((child) => child.data)
-    )()
-    fetch_base_comments_for_story = (story_id) ->
-        response = await fetch('https://oauth.reddit.com/comments/' + story_id, {
+        popular_stories = data.children.map (child) -> {
+            child.data...
+            replies: []
+        }
+        popular_stories.map (story) ->
+            response = await fetch('https://oauth.reddit.com/comments/' + story.id, {
                 method: 'GET'
                 headers:
                     'Authorization': token_type + ' ' + access_token
             })
-        [..., comments] = await response.json()
-        selected_story_comments = comments.data.children.map((child) => child.data)
+            [..., comments] = await response.json()
+            story.replies = comments
+            streamline_reply_datastructs(story)
+    )()
+    streamline_reply_datastructs = (comment) ->
+        if comment.replies?.data?.children
+            if comment.replies.data.children.kind == 'more'
+                comment.replies = []
+            else
+                comment.replies = comment.replies.data.children.map (child) -> child.data
+                for comment in comment.replies
+                    streamline_reply_datastructs comment
+        else
+            comment.replies = []
 </script>
