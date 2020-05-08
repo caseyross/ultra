d347687adcdbdaba550f038648b27cf8590e2005
Casey <caseyros@gmail.com>
2020-05-08 17:16:10 +0000

    add embedded reddit comments display; ui updates

--- App.svelte
+++ App.svelte
@@ -1,27 +1,37 @@
 <template lang="pug">
     #main
-        section#post
-            +if('selected_story.is_self && selected_story.selftext_html')
-                article#story-text(bind:this='{dom.story_text}') {@html decode_reddit_html_entities(selected_story.selftext_html.slice(43, selected_story.selftext_html.length - 34))}
+        section#story-pane
+            +if('selected_story.linked_story')
+                article#story-reddit(bind:this='{dom.story_reddit}')
+                    CommentTree(comment='{selected_story.linked_story}' op_id='{selected_story.linked_story.author_fullname}' focus_comment_id='{selected_story.linked_story.focus_comment_id}')
+                +elseif('selected_story.is_self')
+                    +if('selected_story.selftext_html')
+                        article#story-text(bind:this='{dom.story_text}') {@html decode_reddit_html_entities(selected_story.selftext_html.slice(43, selected_story.selftext_html.length - 34))}
+                        +else
+                            article#story-notext NO TEXT
                 +elseif('selected_story.post_hint === "image"')
                     a(href='{selected_story.url}')
                         img#story-image(src='{selected_story.url}')
                 +elseif('selected_story.post_hint === "hosted:video"')
                     video#story-video(autoplay controls muted='false' src='{selected_story.media.reddit_video.fallback_url}')
-                +else
-                    iframe#story-embedded-page(src='{selected_story.url}' sandbox='allow-scripts allow-same-origin')
+                +elseif('selected_story.url')
+                    iframe#story-embed(src='{selected_story.url}' sandbox='allow-scripts allow-same-origin')
         nav
             header
                 input(type='text' bind:value='{subreddit}' on:change='{load_stories({ count: 8 })}' placeholder='POPULAR')
             #stories-list
                 +each('stories as story')
-                    article.story-brochure(on:click='{select_story(story)}' class:story-brochure-stickied='{story.stickied}' class:story-brochure-read='{read_stories.has(story.id)}' class:story-brochure-selected='{selected_story.id === story.id}')
+                    article.story-brochure(on:mousedown='{select_story(story)}' class:story-brochure-stickied='{story.stickied}' class:story-brochure-read='{read_stories.has(story.id)}' class:story-brochure-selected='{selected_story.id === story.id}')
+                        .story-colorbar
                         h1.story-headline {decode_reddit_html_entities(story.title)}
             footer
-                button(on:mousedown='{load_stories({ count: 8, after: stories[stories.length - 1].name })}') next 8
-        section#comment-area
+                button(on:mousedown='{load_stories({ count: 8, after: stories[stories.length - 1].name })}') +
+        section#comments-pane
             #comments(bind:this='{dom.comments}' on:scroll='{move_minimap_cursor()}' on:mousedown='{teleport_via_minimap}')
-                CommentTree(comment='{selected_story}' op_id='{selected_story.author_fullname}')
+                +if('selected_story.num_comments > 0')
+                    CommentTree(comment='{selected_story}' op_id='{selected_story.author_fullname}')
+                    +elseif('selected_story.num_comments === 0')
+                        #nocomments NO COMMENTS
             figure#minimap(bind:this='{dom.minimap}')
                 canvas#minimap-field(bind:this='{dom.minimap_field}')
                 mark#minimap-cursor(bind:this='{dom.minimap_cursor}')
@@ -31,32 +41,42 @@
     #main
         height: 100%
         display: flex
-        font: bold 16px/1 "Iosevka Aile"
+        font: 400 16px/1 "Iosevka Aile"
         word-break: break-word
-    #post
-        width: 640px
+    #story-pane
+        flex: 1 0 33%
         display: flex
         flex-flow: column nowrap
         justify-content: space-around
         align-items: flex-end
-        background: #222
-        color: #ccc
+        background: #675e56
+        color: white
     nav
-        width: 320px
+        flex: 0 0 17%
         display: flex
         flex-flow: column nowrap
         justify-content: space-between
-        background: #222
+        background: #675e56
         color: white
         user-select: none
-    #comment-area
-        flex: 1
+    #comments-pane
+        flex: 0 1 50%
         contain: strict
         background: #fed
     #comments
         width: 100%
         height: 100%
+        padding-bottom: 16px
         overflow: auto
+    #nocomments
+        width: 100%
+        height: 100%
+        display: flex
+        justify-content: center
+        align-items: center
+        text-align: center
+        font-size: 120px
+        color: wheat
     #minimap
         position: absolute
         top: 0
@@ -74,42 +94,74 @@
         pointer-events: none
     button
     input
-        padding: 24px
+        text-align: center
+        background: #333
         font-size: 32px
-        transition: color 0.1s
-        &:hover
+        font-weight: 900
         &:focus
             outline: none
-            color: royalblue
-        &:active
-            color: cornflowerblue
+            background: #222
     button
-        text-align: right
+        width: 80px
+        height: 80px
+        color: #ccc
+        &:hover
+            background: wheat
+            color: white
     input
         text-transform: uppercase
     #stories-list
-        padding: 24px
         overflow: auto
     .story-brochure
-        padding: 8px 0
         cursor: pointer
-    .story-brochure-stickied
-        color: darkseagreen
-    .story-brochure-read
-        color: gray
+        display: flex
     .story-brochure-selected
-        color: wheat
+        background: #333
+    .story-colorbar
+        flex: 0 0 8px
+        background: white
+        .story-brochure-stickied &
+            background: darkseagreen
+        .story-brochure-read &
+            background: gray
+        .story-brochure-selected &
+            background: wheat
+    .story-headline
+        padding: 8px 16px 8px 16px
+        font-weight: 300
+        .story-brochure-stickied &
+            color: darkseagreen
+        .story-brochure-read &
+            color: gray
+        .story-brochure-selected &
+            color: wheat
+    footer
+        display: flex
+        justify-content: center
+    #story-reddit
+        width: 100%
+        height: 100%
+        overflow: auto
+        background: #fed
+        color: black
     #story-text
         max-height: 100%
         padding: 24px
         overflow: auto
         font-weight: normal
         font-size: 14px
+        line-height: 1.2
+    #story-notext
+        width: 100%
+        text-align: center
+        font-size: 240px
+        color: #333
     a
         max-height: 100%
-    #story-embedded-page
+    #story-embed
         width: 100%
         height: 100%
+        background: white
 </style>
 
 <script type="text/coffeescript">
@@ -117,6 +169,7 @@
     import CommentTree from './CommentTree.svelte'
     export dom =
         story_text: {}
+        story_reddit: {}
         comments: {}
         minimap: {}
         minimap_field: {}
@@ -158,16 +211,33 @@
             child.data...
             replies: []
         }
-        stories.map (story) ->
-            response = await fetch("https://oauth.reddit.com/comments/#{story.id}", {
-                method: 'GET'
-                headers:
-                    'Authorization': "#{token_type} #{access_token}"
-            })
-            [..., comments] = await response.json()
-            story.replies = comments
-            console.log(story)
-            streamline_reply_datastructs story
+        for story in stories
+            load_comments story
+            if story.domain.endsWith('reddit.com') and story.url.split('/')[6]
+                load_linked_story story
+    load_comments = (story) ->
+        response = await fetch("https://oauth.reddit.com/comments/#{story.id}", {
+            method: 'GET'
+            headers:
+                'Authorization': "#{token_type} #{access_token}"
+        })
+        [..., comments] = await response.json()
+        story.replies = comments
+        streamline_reply_datastructs story
+    load_linked_story = (story) ->
+        linked_story_id = story.url.split('/')[6]
+        linked_comment_id = story.url.split('/')[8]
+        linked_comment_context = story.url.split("context=")[1]?.split('&')[0]
+        response = await fetch("https://oauth.reddit.com/comments/#{linked_story_id}?comment=#{linked_comment_id}&context=#{linked_comment_context}", {
+            method: 'GET'
+            headers:
+                'Authorization': "#{token_type} #{access_token}"
+        })
+        [linked_story, linked_comments] = await response.json()
+        story.linked_story = linked_story.data.children[0].data
+        story.linked_story.replies = linked_comments
+        story.linked_story.focus_comment_id = linked_comment_id
+        streamline_reply_datastructs story.linked_story
     streamline_reply_datastructs = (comment) ->
         if comment.body_html
             comment.body_html = decode_reddit_html_entities comment.body_html.slice(22, comment.body_html.length - 13)
@@ -188,8 +258,8 @@
         .replace(/&gt;/g, '>')
     select_story = (story) -> 
         selected_story = story
-        if dom.story_text
-            dom.story_text.scrollTop = 0
+        dom.story_reddit?.scrollTop = 0
+        dom.story_text?.scrollTop = 0
         dom.comments.scrollTop = 0
         read_stories.add story.id
         read_stories = read_stories
@@ -207,14 +277,18 @@
         # Redraw minimap when comments change
         if selected_story.id != memories.previous_story_id or dom.comments.scrollHeight != memories.previous_comments_scrollheight
             # Resize cursor
-            dom.minimap_cursor.style.height = "#{(dom.comments.clientHeight - 34) * dom.comments.clientHeight / dom.comments.scrollHeight}px"
+            if dom.comments.scrollHeight > dom.comments.clientHeight
+                dom.minimap_cursor.style.height = "#{(dom.comments.clientHeight - 34) * dom.comments.clientHeight / dom.comments.scrollHeight}px"
+            else
+                dom.minimap_cursor.style.height = 0
             # Clear minimap symbols
             ctx = dom.minimap_field.getContext '2d'
             ctx.clearRect(0, 0, dom.minimap_field.width, dom.minimap_field.height)
             # Draw new minimap symbols
             ctx.fillStyle = 'gray'
-            for comment in dom.comments.children
-                ctx.fillRect(0, Math.floor(comment.offsetTop / dom.comments.scrollHeight * dom.minimap.clientHeight), dom.minimap.clientWidth, 1)
+            if dom.comments.children.length > 1
+                for comment in dom.comments.children
+                    ctx.fillRect(0, Math.floor(comment.offsetTop / dom.comments.scrollHeight * dom.minimap.clientHeight), dom.minimap.clientWidth, 1)
             memories.previous_story_id = selected_story.id
             memories.previous_comments_scrollheight = dom.comments.scrollHeight
 </script>
--- CommentTree.svelte
+++ CommentTree.svelte
@@ -3,11 +3,11 @@
         +each('comment.replies as reply')
             +if('reply.body')
                 article.comment-tree
-                    .comment
+                    .comment(class:focus-comment='{reply.id === focus_comment_id}')
                         .comment-meta
                             span.author-label(class:author-label-op='{reply.author_fullname === op_id}') {reply.author}
                         .comment-text {@html reply.body_html}
-                    svelte:self(comment='{reply}' op_id='{op_id}')
+                    svelte:self(comment='{reply}' op_id='{op_id}' focus_comment_id='{focus_comment_id}')
 </template>
 
 
@@ -19,8 +19,13 @@
         font-size: 12px
         line-height: 1.2
         word-break: break-word
+    .comment-meta
+        color: #333
     .comment-text
         width: 400px
+        .focus-comment &
+            padding: 8px
+            background: #def
     .author-label
         background: lightgray
     .author-label-op
@@ -30,5 +35,6 @@
 <script type="text/coffeescript">
     export comment =
         replies: []
-    export op_id = undefined
+    export op_id = ''
+    export focus_comment_id = ''
 </script>
