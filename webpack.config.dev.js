c0b174c2e4f9803a07e28c6c8b3eef7ff38753ce
Casey Ross <caseyros@gmail.com>
2022-04-16 20:04:45 -0500

    internals: update webpack dev server config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,16 +9,21 @@ module.exports = {
 				errors: true,
 				warnings: false,
 			},
+			progress: true,
+			reconnect: false,
 		},
+		liveReload: false,
 		headers: {
 			'Link': '<https://oauth.reddit.com>; rel="preconnect", <https://i.redd.it>; rel="preconnect", <https://v.redd.it>; rel="preconnect"',
 			'X-Frame-Options': 'DENY'
 		},
 		historyApiFallback: true, // serve index.html for all routes without designated pages (i.e. all of them)
+		hot: false,
 		https: true,
 		static: {
 			directory: path.join(__dirname, 'dist'),
 		},
+		port: 8080,
 	},
 	mode: 'development',
 }
